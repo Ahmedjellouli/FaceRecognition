f5bce5c2aec9ee7e9dcd367962c0de7edec67d78
Ahmedjellouli <90426606+Ahmedjellouli@users.noreply.github.com>
2021-09-17 01:10:44 +0100

    Update FaceRecognition.py

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,16 +1,21 @@
 import cv2
 from Recognizer import *
 
-# for more details on how to use this code see : https://github.com/Ahmedjellouli/FaceDetection
+# for more details on how to use this code see : https://github.com/Ahmedjellouli/FaceRecognition
 
+Recognizer = Recognizer(Database="Database",
+                        Tolerance=0.55,
+                        detectFrontalFace=False,
+                        detectLandmarks=True)
 
-# Image = Image(Recognizer = Recognizer(Database="empty", Tolerance=0.55, detectFrontalFace=False, detectLandmarks=True),
-#               filename = "Faces\elon.jpg" ,
-#               Save = True )
+Image = Image(Recognizer=Recognizer,
+              filename="Faces\\Malala-Yousafzai.jpg",
+              Save=True)
 
-Video = Video(Recognizer = Recognizer(Database="Database", Tolerance=0.55, detectFrontalFace=False, detectLandmarks=True),
-              filename = "Videos\elon.mp4" ,  # put your image path here e.g : D:\image.jpg
-               )
-Video.RecognizeFaces()   #to detect faces in image
+Video = Video(Recognizer=Recognizer,
+              filename="Videos\elon.mp4",  # put your image path here e.g : D:\image.jpg
+              )
+
+Image.RecognizeFaces()
+Video.RecognizeFaces()  # to detect faces in image
 Video.AddAudio()
-# Image.RecognizeFaces()
